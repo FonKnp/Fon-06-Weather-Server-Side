c62fa542b6c9d17956c5c99592d934be5223eecc
FonKnp <fonnfonk@gmail.com>
2024-03-11 22:09:45 -0400

    add JavaScript variable

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+let searchBt = document.getElementById('search-button');
+let searchInput = document.getElementById('search-input');
+
+let apiKey = '6054fcd953d7cd19d0770921f98b21c1';
+let currentUrl = `https://api.openweathermap.org/data/2.5/forecast?q={city name}&appid=${apiKey}`;
+
+
