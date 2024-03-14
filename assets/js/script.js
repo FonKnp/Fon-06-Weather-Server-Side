51948e51db26d4b669dc6fd09decc3af6e89559b
FonKnp <fonnfonk@gmail.com>
2024-03-14 14:59:24 -0400

    use one fetch to get all forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// let timeDisplayEl = document.getElementById('time-display');
 let searchBt = document.getElementById('search-button');
 let searchInput = document.getElementById('search-input');
 let searchHistory = document.getElementById('search-history');
@@ -13,68 +14,68 @@ let card3 = document.getElementById('card3');
 let card4 = document.getElementById('card4');
 let card5 = document.getElementById('card5');
 
-let cityInput = JSON.parse(localStorage.getItem('city'));
+let cityInput = JSON.parse(localStorage.getItem('city')) || [];
 
 let apiKey = '6054fcd953d7cd19d0770921f98b21c1';
-let weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityInput + '&appid=' + apiKey;
-let forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityInput + '&appid=' + apiKey;
 
-function getweatherUrl () {
+// function displayTime() {
+//   const rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
+//   timeDisplayEl.innerHTML = rightNow;
+// }
+
+function getweatherUrl (city) {
+  let weatherUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + apiKey;
   
   fetch(weatherUrl)
-    .then(function (response) {
-      if (response.ok) {
-        response.json()
-        .then(function(data) {
-          console.log(data);
-          console.log(data.main);
-          console.log(data.main.temp);
-          console.log(data.wind.speed);
-          console.log(data.main.humidity);
-          displayWeather(data);
-        })
-      } else {
-        console.log('Error, not a city name');
-      }
-    })
-    .catch(function (error) {
-      console.log('ERROR Unablr to connect');
-    })
+  .then(function (response) {
+    if (response.ok) {
+      response.json()
+      .then(function(data) {
+        // console.log(data);
+        // console.log(data.list[0]);
+        // console.log(data.list[0].main);
+        // console.log(data.list[0].main.temp);
+        displayWeather(data);
+      })
+    } else {
+      console.log('Error, not a city name');
+    }
+  })
+  .catch(function (error) {
+    console.log('ERROR Unable to connect');
+  })
 }
 
-searchBt.addEventListener('click', function() {
-  // event.preventDefault();
-  addInputValue();
-  getweatherUrl(cityInput);
-  searchInput.value = '';
-});
-
-
-// add city key and value to local storage
-function addInputValue () {
-  let cityName = searchInput.value;
-  
-  localStorage.setItem('city', JSON.stringify(cityName));
-
-}
 
 
 function displayWeather(info) {
   if (info.length === 0) {
     cityCard.textContent = 'Weather information not found!';
   } else {
-    cityName.innerHTML = info.name;
-    currentIcon.src = 'https://openweathermap.org/img/wn/' + info.weather[0].icon +'@2x.png';
-    currentTemp.textContent = 'Temp: ' + ((info.main.temp-273) * (9/5) + 32).toFixed(2) + ' ºF';
-    currentWind.textContent = 'Wind: ' + info.wind.speed + ' MPH';
-    currentHumid.textContent = 'Humidity: ' + info.main.humidity + ' %'
+    cityName.innerHTML = info.city.name;
+    // displayTime();
+    currentIcon.src = 'https://openweathermap.org/img/wn/' + info.list[0].weather[0].icon +'@2x.png';
+    currentTemp.textContent = 'Temp: ' + ((info.list[0].main.temp-273) * (9/5) + 32).toFixed(2) + ' ºF';
+    currentWind.textContent = 'Wind: ' + info.list[0].wind.speed + ' MPH';
+    currentHumid.textContent = 'Humidity: ' + info.list[0].main.humidity + ' %'
   }
 }
 
+
+
 // get data from search history in local storage
 function getSearchHistory () {
-
+  
 }
-
-getweatherUrl();
-
+      
+      
+searchBt.addEventListener('click', function() {
+  
+  let cityEl = searchInput.value;
+  getweatherUrl(cityEl);
+  // getforecastUrl(cityEl);
+  cityInput.push(cityEl);
+  localStorage.setItem('city', JSON.stringify(cityInput));
+  searchInput.value = '';
+  getSearchHistory();
+});
